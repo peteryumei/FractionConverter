17af62306b6ec1fa188f5ece386b08d8b96e7edd
Peter Mei <peter.mei@showmgr.com>
2018-01-22 12:17:41 -0800

    Add project files.

---
+++ FractionConverter/Converter.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionConverter
+{
+    public static class Converter
+    {
+        public static string Convert(decimal pvalue, bool skip_rounding = false)
+        {
+            decimal value = pvalue;
+            decimal dplaces = (decimal)0.03125;
+
+            if (!skip_rounding)
+                value = Converter.DecimalRound(pvalue, dplaces);
+
+            if (value == Math.Round(value, 0)) // whole number check
+                return value.ToString();
+
+            // get the whole value of the fraction
+            decimal mWhole = Math.Truncate(value);
+
+            // get the fractional value
+            decimal mFraction = value - mWhole;
+
+            // initialize a numerator and denomintar
+            uint mNumerator = 0;
+            uint mDenomenator = 1;
+
+            // ensure that there is actual a fraction
+            if (mFraction > 0m)
+            {
+                // convert the value to a string 
+                string strFraction = mFraction.ToString().Remove(0, 2);
+
+                // store the number of decimal places
+                uint intFractLength = (uint)strFraction.Length;
+
+                // set the numerator to have the proper amount of zeros
+                mNumerator = (uint)Math.Pow(10, intFractLength);
+
+                // parse the fraction value to an integer that equals
+                // [fraction value] * 10^[number of decimal places]
+                uint.TryParse(strFraction, out mDenomenator);
+
+                // get the greatest common divisor for both numbers
+                uint gcd = GreatestCommonDivisor(mDenomenator, mNumerator);
+
+                // divide the numerator and the denominator by the greatest common divisor
+                mNumerator = mNumerator / gcd;
+                mDenomenator = mDenomenator / gcd;
+            }
+    
+            StringBuilder mBuilder = new StringBuilder();
+            // add the whole number if it's greater than 0
+            if (mWhole > 0m)
+            {
+                mBuilder.Append(mWhole);
+            }
+
+            // add the fraction if it's greater than 0m
+            if (mFraction > 0m)
+            {
+                if (mBuilder.Length > 0)
+                {
+                    mBuilder.Append(" ");
+                }
+
+                mBuilder.Append(mDenomenator);
+                mBuilder.Append("/");
+                mBuilder.Append(mNumerator);
+            }
+
+            return mBuilder.ToString();
+        }
+
+        private static uint GreatestCommonDivisor(uint valA, uint valB)
+        {
+            if (valA == 0 &&
+              valB == 0)
+            {
+                return 0;
+            }
+            else if (valA == 0 &&
+                  valB != 0)
+            {
+                return valB;
+            }
+            else if (valA != 0 && valB == 0)
+            {
+                return valA;
+            }
+            // actually find the GSD
+            else
+            {
+                uint first = valA;
+                uint second = valB;
+
+                while (first != second)
+                {
+                    if (first > second)
+                    {
+                        first = first - second;
+                    }
+                    else
+                    {
+                        second = second - first;
+                    }
+                }
+
+                return first;
+            }
+        }
+
+        private static decimal DecimalRound(decimal val, decimal places)
+        {
+            string sPlaces = Converter.Convert(places, true);
+            string[] s = sPlaces.Split('/');
+
+            if (s.Count() == 2)
+            {
+                int nPlaces = System.Convert.ToInt32(s[1]);
+                decimal d = Math.Round(val * nPlaces);
+                return d / nPlaces;
+            }
+
+            return val;
+        }
+    }
+
+
+}
---
+++ FractionConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FractionConverter;
+
+namespace FractionConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the decimal number");
+            string input = Console.ReadLine();
+            Decimal inputDecimal;
+            if (Decimal.TryParse(input, out inputDecimal))
+            {
+                string fraction = Converter.Convert(inputDecimal);
+                Console.WriteLine(fraction);
+            }
+            else
+            {
+                Console.WriteLine("The input is not decimal number! ");
+            }
+            Console.WriteLine("Please enter any key to eixt");
+            Console.ReadLine();
+        }
+    }
+}
